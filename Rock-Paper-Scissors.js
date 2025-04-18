9d084e730d9e886bcb72b6cc01bce1d94a7a1e4d
Aditya Pratap <pratapaditya948@gmail.com>
2025-04-18 23:03:03 +0530

    Minor changes in js script like formating and reset-notif

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -34,68 +34,64 @@ return computerMove;
 } */
 
 function playGame(myMove){
-userMove = changeTextToEmoji(myMove);
-const randomNumber = Math.random();
+  userMove = changeTextToEmoji(myMove);
+  const randomNumber = Math.random();
 
-// computer playing      
-if(randomNumber < 1/3){
-  computerMove = 'Rock';
-} else if(randomNumber < 2/3){
-  computerMove = 'Paper';
-} else {
-  computerMove = 'Scissors';
-}
+  // computer playing      
+  if(randomNumber < 1/3){
+    computerMove = 'Rock';
+  } else if(randomNumber < 2/3){
+    computerMove = 'Paper';
+  } else {
+    computerMove = 'Scissors';
+  }
 
-//checking results
-if(computerMove === myMove){
-  result = 'Tie';
-  score.tie++;
-} else if((myMove === 'Rock' && computerMove === 'Paper') || (myMove === 'Paper' && computerMove === 'Scissors') || (myMove === 'Scissors' && computerMove === 'Rock')){
-  result = 'You Lose!!';
-  score.lose++;
-} else {
-  result = 'You Win!!';
-  score.win++;
-}
+  //checking results
+  if(computerMove === myMove){
+    result = 'Tie';
+    score.tie++;
+  } else if((myMove === 'Rock' && computerMove === 'Paper') || (myMove === 'Paper' && computerMove === 'Scissors') || (myMove === 'Scissors' && computerMove === 'Rock')){
+    result = 'You Lose!!';
+    score.lose++;
+  } else {
+    result = 'You Win!!';
+    score.win++;
+  }
 
-localStorage.setItem('score', JSON.stringify(score));
+  localStorage.setItem('score', JSON.stringify(score));
 
-/* alert(`You picked ${myMove}. Computer picked ${computerMove}. \nResult: ${result} \nWins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}`); */
-computerMove = changeTextToEmoji(computerMove);
+  /* alert(`You picked ${myMove}. Computer picked ${computerMove}. \nResult: ${result} \nWins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}`); */
+  computerMove = changeTextToEmoji(computerMove);
 
-updateScoreElements();
+  updateScoreElements();
+  document.querySelector('.reset-notif').innerHTML = '';
 }
 
 function updateScoreElements(){
-
   document.querySelector('.js-score').innerHTML = `Wins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}</p>`;
-
   document.querySelector('.js-result').innerHTML = result;
-
   document.querySelector('.js-moves-window').innerHTML = `You ${userMove} - ${computerMove} Computer`;
-
 }
 
 function resetScore(){
-score.win = 0;
-score.lose = 0;
-score.tie = 0;
-localStorage.removeItem('score');
-result = '';
-userMove = '';
-computerMove ='';
-updateScoreElements();
+  score.win = 0;
+  score.lose = 0;
+  score.tie = 0;
+  localStorage.removeItem('score');
+  result = '';
+  userMove = '';
+  computerMove ='';
+  updateScoreElements();
 
-document.querySelector('.reset-notif').innerHTML = 'Score has been RESET!!';
+  document.querySelector('.reset-notif').innerHTML = 'Score has been RESET!!';
 }
 
 function changeTextToEmoji(Move){
-
-if(Move === 'Rock'){
-  return ` <img class="icons" src="images/rock-emoji.png"> `;
-} else if(Move === 'Paper'){
-  return ` <img class="icons" src="images/paper-emoji.png"> `;
-} else{
-  return ` <img class="icons" src="images/scissors-emoji.png"> `;
-}
+  if(Move === 'Rock'){
+    return ` <img class="icons" src="images/rock-emoji.png"> `;
+  } else if(Move === 'Paper'){
+    return ` <img class="icons" src="images/paper-emoji.png"> `;
+  } else{
+    return ` <img class="icons" src="images/scissors-emoji.png"> `;
+  }
 }
