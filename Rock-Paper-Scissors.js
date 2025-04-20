0a0ede1d8528bd9aea2d8fcae2a6ea82dd7e8602
Aditya Pratap <pratapaditya948@gmail.com>
2025-04-20 18:41:39 +0530

    V2 RPS game. added autoplay feature

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -4,10 +4,6 @@ let score = JSON.parse(localStorage.getItem('score'))||{
   tie:0
 };
 
-let result = '';
-let userMove = '';
-let computerMove = '';
-
 document.querySelector('.js-score').innerHTML = `Wins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}`;
 
 /* if(score === null){
@@ -18,33 +14,24 @@ score = {
 };
 } */
 
-/* function pickComputerMove(){
-const randomNumber = Math.random();
-let computerMove = '';
+function pickRandomMove(){
+  const randomNumber = Math.random();
+  let Move = '';
 
-if(randomNumber < 1/3){
-  computerMove = 'Rock';
-} else if(randomNumber < 2/3){
-  computerMove = 'Paper';
-} else {
-  computerMove = 'Scissors';
-}
+  if(randomNumber < 1/3){
+    Move = 'Rock';
+  } else if(randomNumber < 2/3){
+    Move = 'Paper';
+  } else {
+    Move = 'Scissors';
+  }
 
-return computerMove;
-} */
+  return Move;
+} 
 
 function playGame(myMove){
-  userMove = changeTextToEmoji(myMove);
-  const randomNumber = Math.random();
-
-  // computer playing      
-  if(randomNumber < 1/3){
-    computerMove = 'Rock';
-  } else if(randomNumber < 2/3){
-    computerMove = 'Paper';
-  } else {
-    computerMove = 'Scissors';
-  }
+  let computerMove = pickRandomMove();
+  let result = '';
 
   //checking results
   if(computerMove === myMove){
@@ -62,14 +49,15 @@ function playGame(myMove){
 
   /* alert(`You picked ${myMove}. Computer picked ${computerMove}. \nResult: ${result} \nWins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}`); */
   computerMove = changeTextToEmoji(computerMove);
+  myMove = changeTextToEmoji(myMove);
 
-  updateScoreElements();
+  updateScoreElements(result, myMove, computerMove);
 }
 
-function updateScoreElements(){
+function updateScoreElements(result, myMove, computerMove){
   document.querySelector('.js-score').innerHTML = `Wins: ${score.win}, Losses: ${score.lose}, Ties: ${score.tie}</p>`;
   document.querySelector('.js-result').innerHTML = result;
-  document.querySelector('.js-moves-window').innerHTML = `You ${userMove} - ${computerMove} Computer`;
+  document.querySelector('.js-moves-window').innerHTML = `You ${myMove} - ${computerMove} Computer`;
 }
 
 function resetScore(){
@@ -78,10 +66,25 @@ function resetScore(){
   score.tie = 0;
   localStorage.removeItem('score');
   result = '';
-  userMove = '';
+  myMove = '';
   computerMove ='';
-  updateScoreElements();
+  updateScoreElements(result, myMove, computerMove);
   alert('Score has been reset');
+}
+
+let intervalId;
+
+function autoPlay(){
+  let buttonStatus = document.querySelector('.autoplay-button').innerText;
+  if(buttonStatus === 'Auto Play'){
+    intervalId = setInterval(function(){
+      playGame(pickRandomMove());
+    }, 1000);
+    document.querySelector('.autoplay-button').innerHTML = 'Stop Auto Play';
+  } else {
+    clearInterval(intervalId);
+    document.querySelector('.autoplay-button').innerHTML = 'Auto Play';
+  }
 }
 
 function changeTextToEmoji(Move){
